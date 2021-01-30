9ffea256f660e0b3a35fea728b20457dea3e5273
Rabby Sheikh <iamyahik0.1337@gmail.com>
2021-01-31 00:27:18 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,79 +1,77 @@
-import requests
-import concurrent.futures
-import threading
-import sys
-import os
+from multiprocessing.dummy import Pool
+import requests, re, concurrent.futures, sys, platform, os
+from requests.packages.urllib3.exceptions import InsecureRequestWarning
 
-opl = sys.argv[1]
-
-if not os.path.isdir('results'):
-    print("Bitch Folder Not Exists")
-    os.mkdir('./results/')
-else:
-    print("Bitch Folder Exists")
+requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 
 r = '\x1b[31m'
 g = '\x1b[32m'
-y = '\x1b[33m'
-b = '\x1b[34m'
-m = '\x1b[35m'
-c = '\x1b[36m'
-w = '\x1b[37m'
-Headers = {'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux i686; rv:28.0) Gecko/20100101 Firefox/28.0'}
+
+UA = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
+
+if not os.path.isdir('results'):
+    print("Bitch Folder Not Exists")
+    os.mkdir('results/')
+
+lala = sys.argv[1]
+
+def logo():
+    x = '''
+    ██╗  ██╗███████╗███╗   ██╗████████╗ █████╗ ██╗
+    ██║  ██║██╔════╝████╗  ██║╚══██╔══╝██╔══██╗██║
+    ███████║█████╗  ██╔██╗ ██║   ██║   ███████║██║
+    ██╔══██║██╔══╝  ██║╚██╗██║   ██║   ██╔══██║██║
+    ██║  ██║███████╗██║ ╚████║   ██║   ██║  ██║██║
+    ╚═╝  ╚═╝╚══════╝╚═╝  ╚═══╝   ╚═╝   ╚═╝  ╚═╝╚═╝
+      CMS Checker | Developer @Sh0ya1337 | V1.0'''
+    print("\x1b[1;31m"+x+"\x1b[0m \n")
+
 
 def DetectCMS(site):
-    Joomla = 'http://{}/administrator/help/en-GB/toc.json'.format(site)
-    Joomla2 = 'http://{}/administrator/language/en-GB/install.xml'.format(site)
-    Joomla3 = 'http://{}/plugins/system/debug/debug.xml'.format(site)
-    Joomla4 = 'http://{}/administrator/'.format(site)
-    Opencart2 = 'http://{}/admin/index.php'.format(site)
-    Opencart = 'http://{}/admin/view/javascript/common.js'.format(site)
     try:
-        CheckJom = requests.get(Joomla, timeout=20, headers=Headers).content
-        CheckJom2 = requests.get(Joomla2, timeout=20, headers=Headers).content
-        CheckJom3 = requests.get(Joomla3, timeout=20, headers=Headers).content
-        CheckJom4 = requests.get(Joomla4, timeout=20, headers=Headers).content
-        CheckOpencart = requests.get(Opencart, timeout=20, headers=Headers).content
-        CheckOpencart2 = requests.get(Opencart2, timeout=20, headers=Headers).content
-        if '"COMPONENTS_BANNERS_BANNERS"' in str(CheckJom):
-            with open('./results/joomla-'+opl+'.txt', 'a') as XW:
-                XW.write('http://'+site+'/administrator/index.php \n')
-            print(g+" + "+site+" --> Joomla")
-        elif '<author>Joomla!' in str(CheckJom2):
-            with open('./results/joomla-'+opl+'.txt', 'a') as XW:
-                XW.write('http://'+site+'/administrator/index.php \n')
-            print(g+" + "+site+" --> Joomla")
-        elif '<author>Joomla!' in str(CheckJom3):
-            with open('./results/joomla-'+opl+'.txt', 'a') as XW:
-                XW.write('http://'+site+'/administrator/index.php \n')
-            print(g+" + "+site+" --> Joomla")
-        elif 'content="Joomla!' in str(CheckJom4):
-            with open('./results/joomla-'+opl+'.txt', 'a') as XW:
-                XW.write('http://'+site+'/administrator/index.php \n')
-            print(g+" + "+site+" --> Joomla")
-        elif 'common/login' in str(CheckOpencart2):
-            with open('./results/opencart-'+opl+'.txt', 'a') as XW:
-                XW.write('http://'+site+'/admin/index.php \n')
-            print(g+" + "+site+" --> Opencart")
-        elif 'getURLVar(key)' in str(CheckOpencart):
-            with open('./results/opencart-'+opl+'.txt', 'a') as XW:
-                XW.write('http://'+site+'/admin/index.php \n')
-            print(g+" + "+site+" --> Opencart")
+        Joomla0 = requests.get(site, headers=UA, verify=False, timeout=15)
+        Joomla1 = Joomla0.text
+        Joomla2 = requests.get('{}/administrator/'.format(site), headers=UA, verify=False, timeout=15).text
+        Joomla3 = requests.get('{}/administrator/help/en-GB/toc.json'.format(site), headers=UA, verify=False, timeout=15).text
+        Opencart1 = requests.get('{}/admin/view/javascript/common.js'.format(site), headers=UA, verify=False, timeout=15).text
+        Opencart2 = requests.get('{}/admin/index.php'.format(site), headers=UA, verify=False, timeout=15).text
+        if 'window.Joomla' in Joomla1 or 'content="Joomla!' in Joomla2 or '<author>Joomla!' in Joomla3:
+            print(g+" - "+site+" --> Joomla")
+            open('results/joomla-'+lala+'.txt','a').write(site+'\n')
+        elif '/sites/default/' in Joomla1:
+            print(g+" - "+site+" --> Drupal")
+            open('results/drupal-'+lala+'.txt','a').write(site+'\n')
+        elif 'laravel_session' in Joomla0.cookies:
+            print(g+" - "+site+" --> Laravel")
+            open('results/laravel-'+lala+'.txt','a').write(site+'\n')
+        elif 'catalog/view/' in Joomla1 or 'getURLVar(key)' in Opencart1 or 'common/login' in Opencart2:
+            print(g+" - "+site+" --> Opencart")
+            open('results/opencart-'+lala+'.txt','a').write(site+'\n')
         else:
-            print(r+" + "+site+" --> Unknowm")
-            with open('./results/unknown-'+opl+'.txt', 'a') as XW:
-                XW.write(site + '\n')
+            print(g+" - "+site+" --> Unknown")
+            open('results/unknown-'+lala+'.txt','a').write(site+'\n')
     except:
         print(r+" - "+site+" --> Invalid")
 
+def AdvCheck(domain):
+    try:
+        for X in ['/','/blog','/forum','/forums', '/shop']:
+            ne_a='http://'
+            ne_w=ne_a+domain+X
+            ktn00 = requests.get(ne_w, headers=UA, verify=False, timeout=20).status_code
+            if ktn00==200:
+                DetectCMS(ne_w)
+    except :
+        print(r+" - "+domain+" --> Invalid")
 
 if __name__ == '__main__':
+    logo()
     try:
-        Target = "list/"+opl
+        Target = "list/"+lala
         TEXTList = open(Target, 'r').read().splitlines()
         try:
             with concurrent.futures.ThreadPoolExecutor(200) as executor:
-                executor.map(DetectCMS, TEXTList)
+                executor.map(AdvCheck, TEXTList)
         except Exception as e:
             print(e)
     except Exception as e:
